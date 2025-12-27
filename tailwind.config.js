1f3b091057ad7448777396c6db35d8dbbdc731cd
zayqu <mnyamwezime@gmail.com>
2025-12-27 14:12:36 +0300

    Initialize project using Create React App

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        teal: '#00bcd4',
+        'dark-gray': '#333333',
+      },
+    },
+  },
+  plugins: [],
+}
